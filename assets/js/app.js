7274ebd5353aee9d9c771deaa8734f118d5010ef
narriteration <narriteration@gmail.com>
2017-02-23 23:11:03 -0800

    first round of html and css edits to personal website / added javascript alert

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,4 @@
 // sanity check
 console.log("app.js linked.");
+
+alert("Hello! Welcome to my page! I guess you'll just have to wait until next week so I can replace this annoying JavaScript alert with something much cooler.")
